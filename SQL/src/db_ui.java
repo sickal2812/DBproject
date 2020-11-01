7efeba0dd5820d401f1a2b543bc551727a349ec2
sickal <sickal2812@naver.com>
2020-11-01 16:17:26 +0900

    test1

--- SQL/src/db_ui.java
+++ SQL/src/db_ui.java
@@ -14,6 +14,7 @@ import java.sql.*;
 
 
 public class db_ui {
+	
 	static String msg =" ";
 	static String select_msg = "";
 	static String select_msg2 = "";
@@ -22,6 +23,7 @@ public class db_ui {
 	static String state_msg = "";
 	static int lock = 1;
 	static String pwd = "dlfgns12"; // 이부분만 바꿔주세요
+	static String select_depart = "";
 	
 	public static String DB_Access() throws SQLException, IOException {
 		Connection conn = null;
@@ -43,7 +45,8 @@ public class db_ui {
             System.err.println("드라이버를 로드할 수 없습니다.");
             e.printStackTrace();
         }
-        
+        // lock이 0이면 이것을 실행
+        // db에 select Dname from department 실행하여 Dname들을 가져옴
         if(lock == 0) {
         	String stmt1="select Dname from department";
         	PreparedStatement p=conn.prepareStatement(stmt1);
@@ -71,32 +74,94 @@ public class db_ui {
           count_select++;
         }
         
-        String stmt1="select "+ select + "from "+ from;  //+" where "+"ssn ="+"?";
-        PreparedStatement p=conn.prepareStatement(stmt1);
-        //System.out.println(p);
+        int dno =0;
+        int super_ssn=0;
+        if(msg.contains("Dno")) {
+        	dno =1;
+        	//System.out.println("dno is in");
+        }
+        if(msg.contains("Super_ssn")) {
+        	super_ssn =1;
+        	//System.out.println("super_ssn is in");
+        }
+        String stmt1="";
+        if(dno == 0 && super_ssn==0) {
+        	stmt1="select "+ select + "from "+ from;  //+" where "+"ssn ="+"?";
+        }
+        if(dno == 1 && super_ssn==0) {
+        	select = select.replaceAll("Dno", "");
+        	stmt1="select "+ select + "Dname from department,employee where Dnumber = Dno";
+        	msg = msg.replaceAll("Dno", "Dname");
+        }
+        if(dno == 0 && super_ssn==1) {
+        	select = select.replaceAll(",Super_ssn", "");
+        	select = "f."+select;
+        	select = select.substring(0, select.length()-1);
+        	select = select.replaceAll(",", ",f.");
+        	//System.out.print("find - ");
+        	//System.out.println(select);
+        	stmt1 = "SELECT "+select+" ,CONCAT_WS(' ', e.Fname, e.Minit, e.Lname) from employee e,employee f where e.Ssn = f.super_ssn";
+        	stmt1 = stmt1.replaceAll("f.Super_ssn ,", "");
+        }
+        if(dno == 1 && super_ssn==1) {
+        	// SELECT f.Fname, f.ssn, CONCAT_WS(' ', e.Fname, e.Minit, e.Lname) AS Supervisior, Dname FROM employee e,employee f, department where e.Ssn = f.super_ssn and e.Dno = Dnumber;
+        	select = select.replaceAll(",Super_ssn", "");
+        	select = "f."+select;
+        	select = select.substring(0, select.length()-1);
+        	select = select.replaceAll(",", ",f.");
+        	select = select.replaceAll("f.Super_ssn,","");
+        	//System.out.print("find - ");
+        	//System.out.println(select);
+        	stmt1 = "SELECT "+select+" ,CONCAT_WS(' ', e.Fname, e.Minit, e.Lname), Dname from employee e,employee f, department where e.Ssn = f.super_ssn";
+        	stmt1 = stmt1.replaceAll("f.Dno","");
+        	stmt1 = stmt1.replaceAll(",C","C");
+        	stmt1 = stmt1 + " and e.Dno = Dnumber ";
+        	System.out.println(stmt1);
+        	//System.out.println(select);
+        	//System.out.println(stmt1);
+        }
         
+        if(!select_depart.equals("전체")) {
+        	if(!stmt1.contains("department")) {
+        		stmt1 = stmt1.replaceAll("from", "from department,");
+        	}
+        	if(!stmt1.contains("where")) {
+        		stmt1 = stmt1+ " where Dnumber = dno and Dname = " + '"' + select_depart + '"';
+        	} else {
+        		if(!stmt1.contains("f.")) {
+        			stmt1 = stmt1.replaceAll("where", " where Dnumber = dno and Dname = " + '"' + select_depart + '"' + " and");
+            	} else {
+            		stmt1 = stmt1.replaceAll("where", " where Dnumber = f.dno and Dname = " + '"' + select_depart + '"' + " and "  );
+            	}
+        	}
+        }
+        
+        System.out.println(stmt1);
+        System.out.println("\n\n\n");
+        
+        // select f.ssn ,e.Fname, e.Minit, e.Lname from employee e, employee f where f.Super_ssn = e.Ssn;
+       
+        // SELECT f.Fname, f.Lname, f.ssn, CONCAT_WS(' ', e.Fname, e.Minit, e.Lname) AS Supervisior FROM employee e,employee f where e.Ssn = f.super_ssn;
+        // SELECT Fname,Minit,Lname,ssn, CONCAT_WS(' ', e.Fname, e.Minit, e.Lname) AS Supervisior FROM employee e,employee f where e.Ssn = f.super_ssn
+        
+        
+        // select Dname from department,employee where Dnumber = Dno;
+
+        // select f.Fname, f.Minit, f.Lname, f.ssn ,e.Fname, e.Minit, e.Lname, Dname  from employee e, employee f, department where f.Super_ssn = e.Ssn and f.Dno=Dnumber;
+        
+        // update employee set Salary = "value" where ssn = 'target_ssn';
+
+        PreparedStatement p = conn.prepareStatement(stmt1);
+        //System.out.println(p);
         //System.out.println("Enter a Social Security Number: ");
         //Statement의 첫번째 ?에 넣는다.
         p.clearParameters();
         //System.out.println(p);
-
         //p.setNString(1, where);
         //System.out.println(p);
-        
         ResultSet r=p.executeQuery();
         //System.out.println(r);
-        
-        // select 에 super_ssn 이나 Dno이 들어가있으면
-        // if문 처리해서 super_ssn -> select e.name from employee e, employee f where f.super_ssn = e.ssn
-        // select f.ssn ,e.Fname, e.Minit, e.Lname from employee e, employee f where f.Super_ssn = e.Ssn;
-        // 
-        // dno만 들어간 경우
-        // select /select/ ,Dname from department,employee where Dnumber = Dno;
-        //
-        // dno, super_ssn 둘다 들어간 경우
-        // select f.Fname, f.Minit, f.Lname, f.ssn ,e.Fname, e.Minit, e.Lname, Dname  from employee e, employee f, department where f.Super_ssn = e.Ssn and f.Dno=Dnumber;
-        
-        // update employee set Salary = "value" where ssn = 'target_ssn';
+
         
         while(r.next()){
         	String result = "";
@@ -113,7 +178,6 @@ public class db_ui {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        // db 연결해서 select하는거 어떻게?
 		return stmt1;
 	}
 	
@@ -122,14 +186,14 @@ public class db_ui {
         	
             setTitle("직원 검색용 프로그램");
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            
             this.setLayout(new FlowLayout());
-            
             String total_depart[] = {"전체", "부서별"};
             // deptart에 select DEPARTMENT from department
             lock = 0;
             String department_list = DB_Access();
             lock = 1;
+            // lock 하고 DB_Access를 하게 되면 db에 접속해서 
+            // 가져온 Dname들을 department_list에 넣어서 스크롤바를 만들어줌
             
             department_list = "전체 " + department_list;
             //System.out.println(department_list);
@@ -137,14 +201,16 @@ public class db_ui {
             
             JComboBox<String> combo1 = new JComboBox<String>(total_depart);
             add( combo1, BorderLayout.NORTH);
-
+            
             JComboBox<String> combo2 = new JComboBox<String>(depart);
+            // department_list에는 research같은 값을 넣은적이 없지만, db에서 가져온 dname을 넣어준것
+            
             // String DB_all_depart = select department from * 
             // db의 모든 depart를 불러와서 
             // for  combo2.additem(DB_all_depart[i]); 같이 넣어 줄 수 있을것 같네요
+            
             add(combo2, BorderLayout.NORTH);
             combo2.setEnabled(false);
-            
             JCheckBox chk1 = new JCheckBox("name",false);
             JCheckBox chk2 = new JCheckBox("ssn",false);
             JCheckBox chk3 = new JCheckBox("Bdate",false);
@@ -153,7 +219,6 @@ public class db_ui {
             JCheckBox chk6 = new JCheckBox("Salary",false);
             JCheckBox chk7 = new JCheckBox("super_ssn",false);
             JCheckBox chk8 = new JCheckBox("Dno",false);
-
             this.add(chk1);
             this.add(chk2);
             this.add(chk3);            
@@ -166,25 +231,23 @@ public class db_ui {
             JButton summit_button = new JButton("검색");
             this.add(summit_button);
             // 기본적인 check box 넣기, 검색버튼
+            JButton choose_button = new JButton("선택");
+            this.add(choose_button);
             
             JPanel panel = new JPanel();
-            
-            
-            String[] header = new String[0];
-            String [][] contents = new String[0][];
-            DefaultTableModel model = new DefaultTableModel(contents,header);
-            JTable table =  new JTable(model);
+            String[] header = new String[0];  // 헤더 = columm 설정
+            String [][] contents = new String[0][]; // content는 안에 들어갈 내용물들
+            DefaultTableModel model = new DefaultTableModel(contents,header); // 테이블모델 선언
+            JTable table =  new JTable(model); // 테이블을 만들고 그 안에 바로 앞에 선언한 model을 넣어줌
             JScrollPane jscp1 = new JScrollPane(table);
             jscp1.setPreferredSize (new Dimension(1380,400));
-            panel.add(jscp1);
-            
-            JButton choose_button = new JButton("선택");
-            panel.add(choose_button,BorderLayout.SOUTH); // 왜 아래쪽에 안나오는지..?
+            this.add(jscp1);
 
-            this.add(panel,BorderLayout.CENTER);
-
+            this.add(panel, BorderLayout.WEST);
             JTextArea txt  = new JTextArea(10,80);
             this.add(txt);
+            
+            // 화면구성 
             
             ActionListener summit_listener = new ActionListener() {
 				@Override
@@ -197,6 +260,7 @@ public class db_ui {
 					select_msg = "";
 					select_msg2 = "";
 					count_select = 0;
+					select_depart = "";
 					if(combo1.getSelectedItem().toString() == "전체") {
 		            	combo2.setEnabled(false);
 		            } else {
@@ -245,16 +309,27 @@ public class db_ui {
 					state_msg += "from -" + " " + select_msg + select_msg2 + "\n";
 					txt.setText(state_msg);
 					
+					select_depart = select_msg + select_msg2;
+					
+					
+					
+					if(msg.equals("")) {
+						msg = "Fname,Minit,Lname,ssn,Bdate,Address,Sex,Salary,Super_ssn,Dno";
+					}
+					
 					try {
 						DB_Access();
 						//System.out.println(msg);
 						//System.out.println(db_result);
 						//textbox.append(msg+"\n");
 						//textbox.append(db_result);
+						msg = msg.replaceAll("Super_ssn", "Supervisor");
+						msg = msg.replaceAll("Dno", "Department");
 						String array[] = msg.split(",");
 						String array2[] = db_result.split("\n");
+						
 						for(int i=0; i<array2.length; i++) {
-							System.out.println(array2[i]);
+							//System.out.println(array2[i]);
 						}
 						String[][] array3 = new String[array2.length][];
 						for(int i=0; i<array2.length; i++) {
